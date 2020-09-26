d6e3d2529db27696aeff3232d0c8bce98fb243a7
charan <charan9487@gmail.com>
2020-09-26 15:00:07 +0530

    implemented graphql in shop component

---
+++ src/pages/collection/collection.container.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from "apollo-boost";
+
+import Spinner from "../../components/spinner/spinner.component";
+import CollectionPage from "./collection.component";
+
+const GET_COLLECTION_BY_TITLE = gql`
+            query getCollectionsByTitle($title:String!){
+                        getCollectionsByTitle(title:$title){
+                                    id
+                                    title
+                                    items{
+                                                id
+                                                name
+                                                price
+                                                imageUrl
+                                    }
+                        }
+            }
+ `
+
+const CollectionPageContainer = ({ match }) => {
+            console.log(match.params.collectionId)
+            return (< Query query={GET_COLLECTION_BY_TITLE} variables={{ title: match.params.collectionId }}>
+                        {
+                                    ({ loading, data }) => {
+                                                if (loading) return <Spinner />
+                                                return <CollectionPage collection={data.getCollectionsByTitle} />
+                                    }
+                        }
+            </Query >
+            )
+}
+
+
+export default CollectionPageContainer;
